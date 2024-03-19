bcbb610072bef5d1f91f5043eb91e811f17697a1
D3rKSouL <ewen.kergoat29@gamil.com>
2024-03-19 17:28:21 +0100

    d

--- Class/cmd_rob.cpp
+++ Class/cmd_rob.cpp
@@ -11,62 +11,60 @@ private:
     float angular_z;
 
 public:
+    // Constructeur
     Velocity() : linear_x(0), angular_z(0) {
-        cout << "Objet Velocity créé." << endl;
+        cout << "Création de l'objet Velocity" << endl;
     }
 
-    ~Velocity() {
-        cout << "Objet Velocity détruit." << endl;
+    // Destructeur
+    ~Velocity(){
+        cout << "Destruction de l'objet Velocity" << endl;
     }
 
+    // Afficher les vitesses
     void Display() {
         auto now = chrono::system_clock::now();
         auto now_as_time_t = chrono::system_clock::to_time_t(now);
         auto now_ms = chrono::duration_cast<chrono::milliseconds>(now.time_since_epoch()) % 1000;
+
         cout << put_time(localtime(&now_as_time_t), "%H:%M:%S.") << setfill('0') << setw(3) << now_ms.count()
              << " Vitesse linéaire : " << linear_x << ", Vitesse angulaire : " << angular_z << endl;
     }
 
+    // Avancer avec une certaine vitesse pendant un certain nombre de millisecondes
     void MoveForward(float speed, int milliseconds) {
+        angular_z = 0; // Assurer que le mouvement angulaire est nul
         linear_x = speed;
         Display();
         this_thread::sleep_for(chrono::milliseconds(milliseconds));
     }
 
+    // Tourner avec une certaine vitesse pendant un certain nombre de millisecondes
     void Turn(float speed, int milliseconds) {
+        linear_x = 0; // Assurer que le mouvement linéaire est nul
         angular_z = speed;
         Display();
         this_thread::sleep_for(chrono::milliseconds(milliseconds));
     }
 
+    // Arrêter tous les mouvements
     void Stop() {
         linear_x = 0;
         angular_z = 0;
         Display();
-    }
-
-    // Bonus : Méthode pour déplacer le robot en diagonale
-    void MoveDiagonal(float speed, int milliseconds) {
-        linear_x = speed;
-        angular_z = speed;
-        Display();
-        this_thread::sleep_for(chrono::milliseconds(milliseconds));
-        Stop();
     }
 };
 
 int main(int argc, char **argv) {
     Velocity cmd_vel;
 
-    // Exécution de la séquence principale
-    cmd_vel.MoveForward(1.5, 2000);
-    cmd_vel.Turn(2.7, 1000);
-    cmd_vel.MoveForward(1.5, 2000);
-    cmd_vel.Turn(2.7, 1000);
-    cmd_vel.MoveForward(1.5, 2000);
-
-    // Exécution de l'objectif bonus
-    cmd_vel.MoveDiagonal(1.5, 3000); // Avancer en diagonale
+    // Séquence de mouvements U inversée
+    cmd_vel.MoveForward(1.5, 2000); // Premier segment
+    cmd_vel.Turn(2.7, 1000);         // Premier virage à 90 degrés
+    cmd_vel.MoveForward(1.5, 2000); // Traversée du fond du "U" inversé
+    cmd_vel.Turn(2.7, 1000);         // Second virage à 90 degrés
+    cmd_vel.MoveForward(1.5, 2000); // Dernier segment vertical
+    cmd_vel.Stop();
 
     return 0;
 }
