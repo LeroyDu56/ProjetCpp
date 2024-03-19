6468d95f15117337ec4cf43ad7dcf5e5a7aa578c
LeroyDu56 <socomm56@gmail.com>
2024-03-19 17:07:00 +0100

    g

--- CppEnzo.cpp
+++ CppEnzo.cpp
@@ -15,30 +15,35 @@ public:
         cout << "Creation Velocity" << endl;
     }
 
-    ~Velocity() {
+    ~Velocity(){
         cout << "Destruction Velocity" << endl;
     }
 
     void Display() {
+        // Obtention de l'heure actuelle avec précision jusqu'aux millisecondes
         auto now = chrono::system_clock::now();
         auto now_as_time_t = chrono::system_clock::to_time_t(now);
         auto now_ms = chrono::duration_cast<chrono::milliseconds>(now.time_since_epoch()) % 1000;
+
+        // Affichage de l'heure, des millisecondes et des vitesses
         cout << put_time(localtime(&now_as_time_t), "%H:%M:%S.") << setfill('0') << setw(3) << now_ms.count()
              << " linear x=" << linear_x << " angular z=" << angular_z << endl;
     }
 
     void Avance(float speed, int milliseconds) {
+        // S'assurer que le mouvement angulaire est nul avant de commencer à avancer
+        angular_z = 0;
         linear_x = speed;
         Display();
         this_thread::sleep_for(chrono::milliseconds(milliseconds));
-        Stop();
     }
 
     void Tourne(float speed, int milliseconds) {
+        // S'assurer que le mouvement linéaire est nul avant de commencer à tourner
+        linear_x = 0;
         angular_z = speed;
         Display();
         this_thread::sleep_for(chrono::milliseconds(milliseconds));
-        Stop();
     }
 
     void Stop() {
@@ -46,31 +51,16 @@ public:
         angular_z = 0;
         Display();
     }
-
-    // Méthode pour exécuter le parcours complet
-    void ExecutePath() {
-        MoveForwardAndStop(2000); // Premier segment vertical
-        TurnAndStop(1000);        // Premier virage à 90 degrés
-        MoveForwardAndStop(2000); // Fond du "U" inversé
-        TurnAndStop(1000);        // Deuxième virage à 90 degrés
-        MoveForwardAndStop(2000); // Dernier segment vertical
-    }
-
-private:
-    void MoveForwardAndStop(int milliseconds) {
-        Avance(1.5, milliseconds);
-    }
-
-    void TurnAndStop(int milliseconds) {
-        Tourne(2.7, milliseconds);
-    }
 };
 
 int main(int argc, char **argv) {
     Velocity cmd_vel;
 
-    // Appel de la méthode orchestrant le parcours complet
-    cmd_vel.ExecutePath();
-
-    return 0;
+    // Séquence de mouvements respectant le cycle 2s, 1s, 2s, 1s, 2s
+    cmd_vel.Avance(1.5, 2000); // Premier segment
+    cmd_vel.Tourne(2.7, 1000); // Premier virage à 90 degrés
+    cmd_vel.Avance(1.5, 2000); // Traversée du fond du "U" inversé
+    cmd_vel.Tourne(2.7, 1000); // Second virage à 90 degrés
+    cmd_vel.Avance(1.5, 2000); // Dernier segment vertical
+    cmd_vel.Stop();
 }
