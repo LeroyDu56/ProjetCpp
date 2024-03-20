1f4f7316ed8a3358f4e080a8b2fbcf2c63a607f3
LeroyDu56 <socomm56@gmail.com>
2024-03-20 09:45:32 +0100

    enzo

---
+++ EnzoRos.cpp
@@ -0,0 +1,25 @@
+#include <ros/ros.h>
+#include <sstream>
+#include <geometry_msgs/Twist.h> // Correct the case of the header file
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "EnzoRos");
+    ros::NodeHandle n;
+    // Correct the queue size in advertise method and ensure generic type is properly cased
+    ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 10);
+    ros::Rate loop_rate(1);
+
+    int count = 0;
+    while (ros::ok())
+    {
+        std::stringstream ss;
+        ss << "Cmd#" << count;
+        ROS_INFO("%s", ss.str().c_str());
+
+        ros::spinOnce();
+        loop_rate.sleep();
+        ++count;
+    }
+    return 0;
+}
